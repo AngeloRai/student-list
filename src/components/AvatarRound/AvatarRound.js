e62298d2439acbd420c6434fc682aa030ce74c6a
Angelo Raimondi <angelo.raimondi@globalsys.com.br>
2022-02-18 15:17:59 -0800

    responsiveness adjustments to student card

--- src/components/AvatarRound/AvatarRound.js
+++ src/components/AvatarRound/AvatarRound.js
@@ -3,7 +3,7 @@ const AvatarRound = ({ image, alt }) => {
   return (
     <img
       src={image}
-      className="border-2 rounded-full w-28 h-28 m-2"
+      className="border-2 rounded-full w-28 h-28"
       alt={alt.img}
     />
   );
